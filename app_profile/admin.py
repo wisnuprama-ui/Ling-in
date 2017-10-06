9eca6d1fbca9425293702f80fb162cdd864102e4
Wisnu Pramadhitya <wisnuprama014@gmail.com>
2017-10-06 09:13:25 +0700

    add model to adminsite, test urls in app_profile, change templates in app_dashboard, and change some models setting

--- app_profile/admin.py
+++ app_profile/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import (UserProfile,
+                     Expertise,
+                     ExpertIn)
 
 # Register your models here.
+admin.site.register(UserProfile)
+admin.site.register(Expertise)
+admin.site.register(ExpertIn)
--- app_profile/models.py
+++ app_profile/models.py
@@ -1,42 +1,75 @@
 from django.db import models
-from django.utils import timezone
 from django.contrib.auth.models import User
+
 # Create your models here.
+
+class Expertise(models.Model):
+    """
+    Description:
+    Model for expertise by User
+
+    """
+    expertise = models.CharField(max_length=100)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True, editable=False)
+
+    def __str__(self):
+        return self.expertise
+
+    class Meta:
+        ordering = ('expertise', 'created_at')
 
 class UserProfile(models.Model):
     """
     Description:
     Model for user app
+
     """
     MALE = 'Male'
     FEMALE = 'Female'
-    GENDER_CHOICE = {
-        MALE: 'Male',
-        FEMALE: 'Female'
-    }
+    GENDER_CHOICE = (
+        (MALE,'Male'),
+        (FEMALE, 'Female')
+    )
 
     # basic user profile
-    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
-    username = models.CharField(max_length=150)
-    password = models.CharField(max_length=128) # store password with hashed, not raw password
+    '''
+    For now we don't have to make it as 'real user' that requires to login/logout
+    what we need now is just it's profile
+    '''
+    # user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
 
     # custom
+    username = models.CharField(max_length=128, blank=False, null=False, unique=True)
     first_name = models.CharField(max_length=100)
     middle_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
     email = models.EmailField()
-    photo = models.ImageField(upload_to='images/', blank=True, null=True)
+    photo = models.ImageField(upload_to='media/images/user/', blank=True, null=True)
     birth_date = models.DateField(blank=True, null=True)
     birth_place = models.CharField(max_length=50, blank=True, null=True)
     gender = models.CharField(choices=GENDER_CHOICE, max_length=6)
-
-    # about
     description = models.CharField(max_length=500)
-    #@TODO add expertise
-
 
     created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
+    updated_at = models.DateTimeField(auto_now=True, editable=False)
+
+    def __str__(self):
+        return self.username
 
     class Meta:
-        ordering = ('username', 'name')
+        ordering = ('username', 'first_name', 'middle_name', 'last_name',
+                    'birth_date', 'gender', 'created_at', 'updated_at')
+
+class ExpertIn(models.Model):
+    """
+    Description:
+    Model for bridging user with many expertise
+
+    """
+    user = models.ForeignKey(UserProfile)
+    expertise = models.ForeignKey(Expertise)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True, editable=False)

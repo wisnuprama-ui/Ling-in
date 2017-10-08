9cb8c278486bf1eae3119585a09ac91cd48b4805
Wisnu Pramadhitya <wisnuprama014@gmail.com>
2017-10-08 18:23:53 +0700

    change some url config for app home

--- app_home/urls.py
+++ app_home/urls.py
@@ -3,7 +3,7 @@ from .views import index, login_account, signup_account, create_account
 
 urlpatterns = [
     url(r'^home/$', index, name='homepage'),
-    url(r'^home/login/$', login_account, name='login'),
-    url(r'^home/account/$', signup_account, name='account'),
-    url(r'^home/account/signup/$', create_account, name='signup'),
+    url(r'^account/$', signup_account, name='account'),
+    url(r'^account/login/$', login_account, name='account_login'),
+    url(r'^account/signup/$', create_account, name='account_signup'),
 ]

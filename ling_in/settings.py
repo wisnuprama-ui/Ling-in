25874cc657c04152d3440c78d223fc2d5f24bea2
Wisnu Pramadhitya <wisnuprama014@gmail.com>
2017-10-09 21:08:26 +0700

    change settings and deployment sh

--- ling_in/settings.py
+++ ling_in/settings.py
@@ -83,6 +83,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'USER': 'root_wisnu',
+        'PASSWORD': 'mamamia123',
     }
 }
 

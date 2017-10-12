2aa0e0f1d6c15b4e26962dc7c435bec196a53297
Adyanissa Farsya Kirana <nissakier@gmail.com>
2017-10-12 12:33:01 +0700

    tambahin test, dll di profile

--- app_profile/views.py
+++ app_profile/views.py
@@ -1,20 +1,33 @@
 from django.shortcuts import render, get_object_or_404
-from .models import UserProfile
+from .models import UserProfile, ExpertIn
+from ling_in import strings
 
 # Create your views here.
 
-response = {}
+response = {
+	'TITLE': strings.TITLE,
+	'YEAR': strings.YEAR
+}
 
 def index(request, username=None):
 
 	user = get_object_or_404(UserProfile, username=username)
+	response['page_title'] = "Profile"
 	response['user'] = user
+	query = ExpertIn.objects.filter(user=user)
+	user_expertise = []
+	for exp in query:
+		user_expertise.append(exp.expertise)
+
+	response['user_expertise'] = user_expertise
+
 	template_name = 'app_profile/index_profile.html'
 	return render(request, template_name, response)
 
 def edit(request, username=None):
 
 	user = get_object_or_404(UserProfile, username=username)
+	response['page_title'] = "Edit Profile"
 	response['user'] = user
 	template_name = 'app_profile/profile_edit.html'
 	return render(request, template_name, response)

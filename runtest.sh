ae1c8e963a2a96641b854a205920fad45ff3bfd1
Wisnu Pramadhitya <wisnuprama014@gmail.com>
2017-10-13 02:49:32 +0700

    create instance every runtest and ignore media/img; clean dir; move profile_pic to media

--- runtest.sh
+++ runtest.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-
 echo "Run unit test"
 source env/bin/activate
+echo "from app_profile.models import UserProfile;from django.utils import timezone;UserProfile.objects.filter(username='Anonymous').delete(); username = 'Anonymous';UserProfile(username=username,first_name=username,middle_name=username,last_name=username,email=username + '@' + username + '.com',birth_date=timezone.now(),birth_place=username,gender=UserProfile.MALE).save()" | python manage.py shell
 coverage run --include='app_*/*' manage.py test
 coverage report -m
 

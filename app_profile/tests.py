a3c0fde3708cd91614449a715e65bb510352e995
Adyanissa Farsya Kirana <nissakier@gmail.com>
2017-10-12 09:12:14 +0700

    profile edited

--- app_profile/tests.py
+++ app_profile/tests.py
@@ -1,3 +1,17 @@
 from django.test import TestCase
+from django.test import Client
+from .views import index, edit
 
-#class ProfileUnitTest(TestCase):
+class ProfileUnitTest(TestCase):
+
+	def test_url_profile_is_exist(self):
+		response= Clent().get('/app_profile/')
+		self.assertEqual(response.status_code, 200)
+
+	def test_profile_using_index_func(self):
+		found = resolve('/app_profile/')
+		self.assertTrue(found.func, index)
+
+	#def test_profile_edited_using_edit_func(self):
+
+
